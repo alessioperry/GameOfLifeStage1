a2fabbc0b7a2be163382a265c53da95f80e9d11d
alessioperry <ale.pam@gmail.com>
2013-12-14 13:46:06 +0100

    Added project
    missmatch

---
+++ GameOflifeStage1.Business/Cell.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GameOflifeStage1.Business
+{
+    public class Cell
+    {
+        private IEnumerable<Cell> neighbours;
+        private State state;
+
+        public Cell()
+        {
+
+        }
+
+
+        public State Tick()
+        {
+            var result = neighbours.Count(cell => cell.state == State.Alive);
+
+            if (result == 2 || result == 3)
+                return State.Alive;
+
+            return State.Dead;
+        }
+
+    }
+
+    public enum State
+    {
+        Alive,
+        Dead,
+    }
+}
---
+++ GameOflifeStage1.Business/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOflifeStage1.Business
+{
+    public class Class1
+    {
+    }
+}
