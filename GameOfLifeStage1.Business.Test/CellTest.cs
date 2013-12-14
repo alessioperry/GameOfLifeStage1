97bc8d7899689012fe102dc6a04bdb4d55b1689d
alessioperry <ale.pam@gmail.com>
2013-12-14 10:53:37 +0100

    Added first test project and some packages form nuget.

---
+++ GameOfLifeStage1.Business.Test/CellTest.cs
@@ -0,0 +1,42 @@
+﻿using GameOflifeStage1.Business;
+using NUnit.Framework;
+
+namespace GameOfLifeStage1.Business.Test
+{
+    [TestFixture]
+    public class CellTest
+    {
+
+        public Cell sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            sut = new Cell();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            sut = null;
+        }
+
+        [Test]
+        public void EachAliveCellDeadIfItHaveLessThenTwoAliveNeighbours()
+        {
+            Assert.AreEqual(sut.Tick(), State.Dead);
+        }
+
+        [Test]
+        public void EachAliveCellDeadIfItHaveMuchThenTreeAliveNeighbours()
+        {
+            Assert.AreEqual(sut.Tick(), State.Dead);
+        }
+
+        [Test]
+        public void EachAliveCellRemainAliveIfHeHadTworTreeAliveNeighbours()
+        {
+            Assert.AreEqual(sut.Tick(), State.Alive);
+        }
+    }
+}
